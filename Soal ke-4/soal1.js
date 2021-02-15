faa5063c67162ac62bb3a54cfa1ace5715e55eb1
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-02-16 00:17:59 +0700

    2.Edit Promise

--- Soal ke-4/soal1.js
+++ Soal ke-4/soal1.js
@@ -1,3 +1,4 @@
+//Promise 1
 let ditepati =true;
 const janji = new Promise((resolve,reject)=>{
   if(ditepati){
@@ -6,7 +7,50 @@ const janji = new Promise((resolve,reject)=>{
       reject('janji tidak di tepati');
   }
 });
-// console.log(janji);
 janji
 .then(result => console.log(`OK! : ${result}`))
-.catch(result=>console.log(`NOT OK: ${result}`)) 
+.catch(result=>console.log(`NOT OK: ${result}`))  
+//Promise 2
+let ditepati1 =true;
+const janji1 = new Promise((resolve,reject)=>{
+  if(ditepati1){
+    setTimeout(()=>{
+      resolve('janji di tepati');
+    },2000)
+  }else{
+    setTimeout(()=>{
+      reject('janji tidak di tepati');
+    },2000)
+  }
+});
+janji1
+.finally(()=>console.log('selesai '))
+.then(result => console.log(`OK! : ${result}`))
+.catch(result=>console.log(`NOT OK: ${result}`))
+
+//Promise 3
+const film = new Promise (resolve=>{
+  setTimeout(()=>{
+    resolve([{
+      judul : 'PowerRangers',
+      sutradara: 'plugin007',
+      pameran:'Promise'
+    }])
+  },1000)
+});
+const cuaca = new Promise (resolve=>{
+  setTimeout(()=>{
+    resolve([{
+      kota : 'Tegal',
+      temp: 26,
+      kondisi:'Badai Tornado'
+    }])
+  },1000)
+});
+
+Promise.all([film,cuaca])
+  .then(response =>{
+    const [film,cuaca] = response;
+    console.log(film);
+    console.log(cuaca);
+  })
